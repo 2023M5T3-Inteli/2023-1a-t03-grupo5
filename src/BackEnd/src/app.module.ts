e4d35334a72f3d50cc171dfa8e4c5931332bc555
pepehaggeb <99206621+PedroHaggeBaptista@users.noreply.github.com>
2023-02-20 23:27:35 -0300

    update files

--- src/BackEnd/src/app.module.ts
+++ src/BackEnd/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProjectsModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

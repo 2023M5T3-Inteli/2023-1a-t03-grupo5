ea850c2879bbb5df18df7442a58164758a7fe944
vict0rcarvalh0 <victordecarvalho@outlook.pt>
2023-02-24 11:53:44 -0300

    Test Driven Development

--- src/BackEnd/src/modules/projects/projects.service.spec.ts
+++ src/BackEnd/src/modules/projects/projects.service.spec.ts
@@ -15,4 +15,157 @@ describe('ProjectsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+
+  // Unit test for the "get all projects" endpoint function
+  it("Should return all the projects", () => {
+    var projects = ["Projeto Python", "Projeto Java", "Projeto Javascript"]
+    expect(service.getProjects().length).toBe(projects.length)
+  });
+
+  // Unit test for the "create new project" endpoint function
+  it ("Should create new project", () => {
+    let projectCreated = {
+      name: "Desenvolvimento de aplicação",
+      start: "10/10/2023",
+      end: "10/11/2023",
+      description: "Desenvolvimento de aplicação para notebooks dell",
+      ownerId: 20,
+      coleaderId: 56,
+      projectType: "Dev",
+      blockedSubscriptions: true
+    }
+    expect(service.createProject("Desenvolvimento de aplicação", "10/10/2023", "10/11/2023", "Desenvolvimento de aplicação para notebooks dell", 20, 56, "Dev", true)).toBe(projectCreated)
+  })
+
+  // Unit test for the "delete project" endpoint function
+  it("Should delete project", () => {
+        const projectIdToDelete = 1;
+        const beforeDelete = service.getProjects();
+        service.deleteProject(projectIdToDelete);
+        const afterDelete = service.getProjects();
+        expect(afterDelete.length).toBe(beforeDelete.length - 1);
+        expect(afterDelete.some(project => project.id === projectIdToDelete)).toBe(false);
+    });
+  
+  // Unit test for the "edit already created project" endpoint function
+  it('Should edit a project', () => {
+    let newProjectData = {
+      name: "Desenvolvimento de aplicação",
+      start: "10/10/2023",
+      end: "10/11/2023",
+      description: "Desenvolvimento de aplicação para notebooks dell",
+      ownerId: 20,
+      coleaderId: 56,
+      projectType: "Dev",
+      blockedSubscriptions: true
+    }
+    expect(service.editProject(1, newProjectData)).toEqual({
+      id: 1,
+      name: "Desenvolvimento de aplicação",
+      start: "10/10/2023",
+      end: "10/11/2023",
+      description: "Desenvolvimento de aplicação para notebooks dell",
+      ownerId: 20,
+      coleaderId: 56,
+      projectType: "Dev",
+      blockedSubscriptions: true
+    })
+  })
+
+  // Unit test for the "edit already created specific project" endpoint function
+  it('Should edit a specifc project information', () => {
+    let newProjectData = {
+      blockedSubscriptions: false
+    }
+    expect(service.editProject(1, newProjectData)).toEqual({
+      id: 1,
+      name: "Desenvolvimento de aplicação",
+      start: "10/10/2023",
+      end: "10/11/2023",
+      description: "Desenvolvimento de aplicação para notebooks dell",
+      ownerId: 20,
+      coleaderId: 56,
+      projectType: "Dev",
+      blockedSubscriptions: false
+    })
+  })
+
+  // Unit test for the "approving a project" endpoint function
+  it('Should approve a project', () => {
+    let newApproveData = {
+      projectId: 23,
+      ownerId: 20,
+      approve: true
+    }
+    expect(service.approveProject(newApproveData)).toEqual({
+      projectId: 23,
+      ownerId: 20,
+      approve: true
+    })
+  })
+
+  // Unit test for the "get project by ID" endpoint function
+  it('Should return a project by its ID', () => {
+    const projectId = 1;
+
+    const projectToReturn = {
+        id: projectId,
+        name: 'Desenvolvimento de aplicação',
+        start: '2022-01-01',
+        end: '2022-12-31',
+        description: 'Desenvolvimento de aplicação para notebooks dell',
+        ownerId: 1,
+        coleaderId: 2,
+        projectType: 'Dev',
+        blockedSubscriptions: false,
+    };
+
+    expect(service.getProject(projectId)).toBe(projectToReturn)
+  });
+
+  // Unit test for the "apply project vacancy" endpoint function
+  it('Should apply for a project vacancy', () => {
+    let apply = {
+      applicantId: 1,
+      projectId: 2,
+      roleId: 4,
+      why: "Eu desejo para aprimorar minhas habilidade",
+      habilities: "programaçao"
+    }
+
+    expect(service.applyProject(apply)).toEqual("Aplicação feita com sucesso!")
+  })
+
+  //Unit test for the "get specific apply" endpoint function
+  it('Should get a specific apply', () => {
+    expect(service.getApply(1)).toEqual({
+      id: 1,
+      applicantId: 1,
+      projectId: 2,
+      roleId: 4,
+      why: "Eu desejo para aprimorar minhas habilidade",
+      habilities: "Programaçao"
+    })
+  })
+
+  // Unit test for the "get all applies" endpoint function
+  it('Should all applies', () => {
+    expect(service.getAllApplies()).toEqual([
+      {
+        id: 1,
+        applicantId: 1,
+        projectId: 2,
+        roleId: 4,
+        why: "Eu desejo para aprimorar minhas habilidade",
+        habilities: "programaçao"
+      },
+      {
+        id: 2,
+        applicantId: 3,
+        projectId: 6,
+        roleId: 5,
+        why: "Eu desejo aprender mais sobre programação",
+        habilities: "UX"
+      }
+    ])
+  })
--- src/BackEnd/src/modules/users/users.service.spec.ts
+++ src/BackEnd/src/modules/users/users.service.spec.ts
@@ -15,4 +15,47 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // Unit test for the "get all users" endpoint function
+  it('Should return all users', () => {
+    expect(service.getUsers()).toEqual([
+      { id: 1, name: 'test', email: 'test@test', password: 'test1234' },
+      { id: 2, name: 'test', email: 'test2@test', password: 'test1234' }
+    ])  
+  });
+
+  // Unit test for the "get a specific user by id" endpoint function
+  it('Should return a user', () => {
+    expect(service.getUser("id")).toEqual([
+      { id: 1, name: 'test', email: 'test@test', password: 'test1234' }
+    ])
+  });
+
+  // Unit test for the "create user" endpoint function
+  it('Should create a user', () => {
+    expect(service.createUser("name", "email", "password")).toEqual([
+      { id: 3, name: 'name', email: 'email', password: 'password' }
+    ])
+  });
+
+  // Unit test for the "update user" endpoint function 
+  it('Should update a user', () => {
+    expect(service.updateUser("id", "name", "email", "password")).toEqual([
+      { id: 1, name: 'testing', email: 'email@test', password: 'password' }
+    ])
+  });
+
+  // Unit test for the user login endpoint function
+  it('Should login a user', () => {
+    expect(service.login("email", "password")).toEqual(
+      "User logged in"
+    )
+  });
+
+  // Unit test for the user authentication endpoint function
+  it('Should autenticate a user', () => {
+    expect(service.authenticate("email", "password")).toEqual(
+      "User authenticated"
+    )
+  });
 });

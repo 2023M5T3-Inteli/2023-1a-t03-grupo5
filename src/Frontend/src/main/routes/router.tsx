0a106fc74729ef4fefe0c2255565c94c6c7a139d
Delistoianov <joao.gonzalez@sou.inteli.edu.br>
2023-03-09 17:50:49 -0300

    tela inscrição

--- src/Frontend/src/main/routes/router.tsx
+++ src/Frontend/src/main/routes/router.tsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from '../../presentation/components/partials/navbar/navbar'
 import Home from '../../presentation/pages/home/home'
 // import Login from '../../presentation/pages/login/login'
 import CreateProject from '../../presentation/pages/createProject/createProject'
 import Login from '../../presentation/pages/login/login'
+import ApplicationForm from '../../presentation/pages/applicationForm/applicationForm'
 
 const Router: React.FC = () => {
-  const [active, setActive] = useState(0)
+  const [active, setActive] = useState(-1)
+  const [path, setPath] = useState(window.location.href)
 
   const changePage = (index: number) => {
     setActive(index)
   }
 
+  useEffect(() => {
+    setPath(window.location.href)
+  }, [changePage, path])
+
   return (
     <BrowserRouter>
-      <Navbar page={active} changePage={changePage} />
+      {
+        !path.includes('login') && <Navbar page={active} changePage={changePage} />
+      }
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
+        <Route path='/login' element={<Login changePage={changePage} />}></Route>
+        <Route path='/applicationForm' element={<ApplicationForm />}></Route>
         {/* <Route path='/login' element={<Login />}></Route> */}
         <Route path='/CreateProject' element={<CreateProject />}></Route>
       </Routes>

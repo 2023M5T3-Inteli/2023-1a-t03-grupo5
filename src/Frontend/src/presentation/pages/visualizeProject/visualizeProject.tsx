8f217a0edbe06c3f046648f44174960440aa3150
mariana2903 <mariana.paula@sou.inteli.edu.br>
2023-03-09 21:53:13 -0300

    visualize project

--- src/Frontend/src/presentation/pages/visualizeProject/visualizeProject.tsx
+++ src/Frontend/src/presentation/pages/visualizeProject/visualizeProject.tsx
@@ -3,24 +3,53 @@ import Input from "../../components/Input";
 import "./visualizeProject-styles.scss";
 
 const VisualizeProject: React.FC = () => {
-    return (
-        <div className="">
-            <div className="grid-6 visualize-project container-visualize">
-                <h1>Project name</h1>
-                <h3>Prazo para Inscrição: xx/xx/xx </h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <h2>Necessary Skills</h2>
+  return (
+    <div className="visualize-project">
+      <div className="grid-7 container-visualize">
+        <div className="info-section">
+          <h1 className="name-project">Project name</h1>
+          <h2>Prazo para Inscrição: xx/xx/xx </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <div className="skills-necessary">
+            <h2 className="necessary-skills">Necessary Skills</h2>
+            <div className="grid-4 ">
+                <p className="skills-visualize">javaScript</p>
             </div>
-            <div className="grid-6 info-project">
-                <h2>Project owner</h2>
-                <p>Mariana Silva</p>
-                <h3>Project area</h3>
-                <p>desenvolvimento</p>
-                <h3>Expect date</h3>
-                <p>xx/xx/xxxx</p>
+            <div className="grid-4">
+				<p className="skills-visualize">Python</p>
             </div>
+            <div className="grid-4 ">
+                <p className="skills-visualize">C++</p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="grid-5 info-project">
+        <div className="project-owner-info">
+          <h2 className="h2-owner">Project owner</h2>
+          <p className="p-owner">Mariana Silva</p>
+          <h4>Project area</h4>
+          <p className="info-owner-p">desenvolvimento</p>
+          <h4>Expect date</h4>
+          <p className="info-owner-p">xx/xx/xxxx</p>
+        </div>
+      </div>
+      <div className="grid-12 container-button">
+          <button className="button-visuproject">Vacancies</button>
+        </div>
+    </div>
+    
+  );
+};
 
 export default VisualizeProject;

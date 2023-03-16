17f521e71a42c01ad224bb6b2d477ec77b2583e7
pepehaggeb <99206621+PedroHaggeBaptista@users.noreply.github.com>
2023-03-16 00:38:09 -0300

    Refact: Card Home Page

--- src/Frontend/src/presentation/components/card/card.tsx
+++ src/Frontend/src/presentation/components/card/card.tsx
@@ -7,15 +7,33 @@ const Card: any = (props: any) => {
   console.log(props)
   return (
     <div className='card'>
-      <div className="container">
-        <h2 className='title'>{props.title}</h2>
-        <p className='description'>{props.description}</p>
-        <span className='vacancies'>
-          {props.vacancies}
-          {/* <span className='info'>X vancancies to devs</span> */}
-        </span>
-        <span className='date'>Schedule to start: {props.startDate}</span>
+      <div className='bodyCard'>
+        <div className="container">
+          <div className='grid-8 nameProject'>
+            <h2 className='title'>{props.title}</h2>
+            <p className='description'>{props.description}</p>
+          </div>
+          <div className='grid-4 status'>
+            <div className='people'>
+              <p>50</p>
+              <span><img width={24} src='../../../public/user.png'></img></span>
+            </div>
+            <div className='statusDiv'>
+              <p>Status</p>
+              <span className='greenCircle'></span>
+            </div>
+          </div>
+        </div>
+        <div className='moreDetails'>
+          <div className='detailsLeft'>
+            <p className='moreDetailsText'>More Details</p>
+          </div>
+          <div className='detailsRight'>
+            <div><img src='../../../public/arrow-right.png'></img></div>
+          </div>
+        </div>
       </div>
+      
     </div>
   )
 }

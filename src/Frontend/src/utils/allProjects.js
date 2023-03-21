e02e691395889241820a71230bc0300f90325dc9
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-03-21 16:47:40 -0300

    Update allProjects.js

--- src/Frontend/src/utils/allProjects.js
+++ src/Frontend/src/utils/allProjects.js
@@ -11,9 +11,41 @@ const getProjects = async () => {
         .then((response)=> {
             //create a new array with the data from the response
             const projects = [];
+            //loop through the response and push the data to the array
             response.data.forEach((project) => {
                 projects.push(project);
             });
         });
-        
+
+        document.getElementById("projects").innerHTML = "";
+
+        //render the projects
+        renderProjects();
     }
+
+const renderProjects = (list) => {
+    const table = document.getElementById("projects");
+
+    list.length > 0 
+    ? list.map((project) => {
+        const {id, name} = project;
+        table.innerHTML += `
+        <tr>
+            <td>${id}</td>
+            <td>${name}</td>
+            <Link to={'/project/${id}'}>
+            <button id="confirma-botao" onClick={() => {
+            handleInput();
+            }}>Projeto</button>
+        </Link>
+        </tr>
+        `;
+    })
+    : table.innerHTML += `
+    <tr>
+        <td colspan="2">Nenhum projeto encontrado</td>
+    </tr>
+    `;
+}
+
+

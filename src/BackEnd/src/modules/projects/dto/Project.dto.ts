9f7e436d3cc30de19651b59836b0443961ded69f
vict0rcarvalh0 <victordecarvalho@outlook.pt>
2023-03-21 14:02:28 -0300

    refactor: adjusting dto class validator

--- src/BackEnd/src/modules/projects/dto/Project.dto.ts
+++ src/BackEnd/src/modules/projects/dto/Project.dto.ts
@@ -1,97 +1,87 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsBoolean, IsDate } from "class-validator";
-
-
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsBoolean, IsDate } from 'class-validator';
 
 export class ProjectDTO {
+  @ApiProperty({
+    description: 'Project ID',
+    example: '12345',
+  })
+  projectId: string;
 
-    @ApiProperty({
-        description: 'Project ID',
-        example: '12345',
-    }
-    )
-    
-    projectId: string 
-    
-    @ApiProperty({
-        description: 'Project name',
-        example: 'Desenvolvimento de aplicação',
-    })
-    @IsString()
-    name: string
+  @ApiProperty({
+    description: 'Project name',
+    example: 'Desenvolvimento de aplicação',
+  })
+  @IsString()
+  name: string;
 
-    @ApiProperty({
-        description: 'Project description',
-        example: '10/10/2023',
-    })
-    @IsString()
-    @IsDate()
-    createdAt: Date
+  @ApiProperty({
+    description: 'Project description',
+    example: '10/10/2023',
+  })
+  @IsString()
+  createdAt: Date;
 
-    @ApiProperty({
-        description: 'Project description',
-        example: '10/10/2023',
-    })
-    @IsString()
-    @IsDate()
-    updatedAt: Date
+  @ApiProperty({
+    description: 'Project description',
+    example: '10/10/2023',
+  })
+  @IsString()
+  updatedAt: Date;
 
-    @ApiProperty({
-        description: 'Project description',
-        example: 'o projeto e sobre desenvolvimento de uma aplicação',
-    })
-    @IsString()
-    description: string
+  @ApiProperty({
+    description: 'Project description',
+    example: 'o projeto e sobre desenvolvimento de uma aplicação',
+  })
+  @IsString()
+  description: string;
 
-    @ApiProperty({
-        description: 'Project tasks',
-        example: 'tarefas',
-    })
-    @IsString()
-    tasks: string
+  @ApiProperty({
+    description: 'Project tasks',
+    example: 'tarefas',
+  })
+  @IsString()
+  tasks: string;
 
-    @ApiProperty({
-        description: 'Project start date',
-        example: '10/10/2023',
-    })
-    @IsString()
-    @IsDate()
-    start: Date
+  @ApiProperty({
+    description: 'Project start date',
+    example: '10/10/2023',
+  })
+  @IsString()
+  start: Date;
 
-    @ApiProperty({
-        description: 'Project end date',
-        example: '10/10/2023',
-    })
-    @IsString()
-    @IsDate()
-    end: Date
+  @ApiProperty({
+    description: 'Project end date',
+    example: '10/10/2023',
+  })
+  @IsString()
+  end: Date;
 
-    @ApiProperty({
-        description: 'Project coleader ID',
-        example: '56',
-    })
-    @IsString()
-    coleaderId: string
+  @ApiProperty({
+    description: 'Project coleader ID',
+    example: '56',
+  })
+  @IsString()
+  coleaderId: string;
 
-    @ApiProperty({
-        description: 'Project owner ID',
-        example: '20',
-    })
-    @IsString()
-    ownerId: string
+  @ApiProperty({
+    description: 'Project owner ID',
+    example: '20',
+  })
+  @IsString()
+  ownerId: string;
 
-    @ApiProperty({
-        description: 'Project type',
-        example: 'dev',
-    })
-    @IsString()
-    projectType: string
+  @ApiProperty({
+    description: 'Project type',
+    example: 'dev',
+  })
+  @IsString()
+  projectType: string;
 
-    @ApiProperty({
-        description: 'Project blocked subscription',
-        example: 'true',
-    })
-    @IsString()
-    @IsBoolean()
-    blockedSubscription: boolean
-}                
+  @ApiProperty({
+    description: 'Project blocked subscription',
+    example: 'true',
+  })
+  @IsBoolean()
+  blockedSubscription: boolean;
+}

82027e9bc4acd5e049c0bc435ed8d93405c0afb0
vict0rcarvalh0 <victordecarvalho@outlook.pt>
2023-03-23 16:53:58 -0300

    feat: user service - integration functions

--- src/FrontEnd/src/main/services/userService.ts
+++ src/FrontEnd/src/main/services/userService.ts
@@ -1,19 +1,63 @@
-import axios from "axios"
+import axios from "axios";
 
 const PORT = 3001;
-const HOST = 'localhost';
+const HOST = "localhost";
 const API_URL = `http://${HOST}:${PORT}`;
 
 const UserService = {
   findByID: async (id: String) => {
     try {
-      const response = await axios.get(`${API_URL}/User/Info/${id}`)
-      return response.data
+      const response = await axios.get(`${API_URL}/user/info/${id}`);
+      return response.data;
+    } catch (error) {
+      return [];
     }
-    catch (error) {
-      return []
-    }
-  }
-}
+  },
 
-export default UserService
+  findAll: async () => {
+    try {
+      const response = await axios.get(`${API_URL}/user/find-all`);
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  update: async (id: String, data: any) => {
+    try {
+      const response = await axios.put(`${API_URL}/user/update/${id}`, data);
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  create: async (data: any) => {
+    try {
+      const response = await axios.post(`${API_URL}/user/create`, data);
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  delete: async (id: String) => {
+    try {
+      const response = await axios.delete(`${API_URL}/user/delete/${id}`);
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  getByName: async (name: String) => {
+    try {
+      const response = await axios.get(`${API_URL}/user/find-by-name/${name}`);
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+};
+
+export default UserService;

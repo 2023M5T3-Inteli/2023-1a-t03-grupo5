96b442ce8946ff7c36f5fc778f4653d209bab8ea
pingu01 <99196159+pingu01@users.noreply.github.com>
2023-03-23 17:33:17 -0300

    Create registrations.tsx

---
+++ src/Frontend/src/presentation/pages/registrations/registrations.tsx
@@ -0,0 +1,50 @@
+import "./registrations-styles.scss";
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+
+let Registrations = () => {
+    const 
+
+    return (
+        <div className="bodyRegistrations">
+            <div className="containerRegistrations">
+                <div className="containerRegistrationsTitle">
+                    <h1 className="title">Registrations</h1> 
+                    <div><img src = {"/user.png"}/></div>
+                </div>
+            
+
+                    <div className="filterDiv">
+                        <div className="filterInputDiv">
+                            <input className="inputFilter" placeholder="Search by name"></input>
+                            <div className="iconInputFilter"><img width={28} src={"/search.png"}/></div>
+                        </div>
+                        <div className="filterInputDiv">
+                            <input className="inputFilter" placeholder="Filter"></input>
+                            <div className="iconInputFilter"><img width={28} src={"/filter.png"}/></div>
+                        </div>                    
+                    </div>
+
+                    <div className="registrationsTable">
+                        <div className="container">
+                            <div className="containerTable">
+                                <div className="firstContainer">
+                                    <p className="nameRegistry">1. Felipe Saladys</p>
+                                    <button className="Approve">Approve</button>
+                                    <button className="Refuse">Refuse</button>
+                                    </div>
+                                    <div className="seeProfile">
+                                                <button className="buttonSeeProfile">See Profile</button>
+                                            </div>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Registrations;
